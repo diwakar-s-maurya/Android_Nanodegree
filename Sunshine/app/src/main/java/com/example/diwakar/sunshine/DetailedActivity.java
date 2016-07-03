62fa9e8b289f207d73860d0a6c5e6d2f7390b725
diwakar <diwakar.s.maurya@gmail.com>
2016-07-03 17:05:24 +0530

    add detailed activity

---
+++ Sunshine/app/src/main/java/com/example/diwakar/sunshine/DetailedActivity.java
@@ -0,0 +1,69 @@
+package com.example.diwakar.sunshine;
+
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class DetailedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed);
+
+        Intent intent = getIntent();
+        String data = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (data != null)
+            ((TextView) findViewById(R.id.details)).setText(data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+//        if (mShareActionProvider != null) {
+//            mShareActionProvider.setShareIntent(createShareForecastIntent());
+//        } else {
+//            Log.d("DetailedActivity", "Share Action Provider is null?");
+//        }
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        String text = ((TextView) findViewById(R.id.details)).getText().toString();
+        sendIntent.putExtra(Intent.EXTRA_TEXT, text);
+        sendIntent.setType("text/plain");
+        return sendIntent;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.v("Diwakar", "option clicked");
+        switch (item.getItemId()) {
+            case R.id.action_share:
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+                String text = ((TextView) findViewById(R.id.details)).getText().toString();
+                sendIntent.putExtra(Intent.EXTRA_TEXT, text);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
